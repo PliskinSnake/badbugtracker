93922713217f633b545f94d179dcab31f51fc3ac
dmitriisilin <pliskinsnake5@gmail.com>
2021-11-22 23:55:51 +0300

    Initial Commit, empty project

---
+++ src/main/java/com/dmitrii/silin/badbugtracker/BadbugtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dmitrii.silin.badbugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadbugtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadbugtrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dmitrii/silin/badbugtracker/BadbugtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmitrii.silin.badbugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadbugtrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
